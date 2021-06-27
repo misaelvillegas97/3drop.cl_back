3e5d3deac2e5837e02e549339ebade6cdd2fb5c6
David Misael Villegas Sandoval <david.misa97@gmail.com>
2021-06-27 06:08:00 -0400

    Added password_hash to env config

--- src/config.ts
+++ src/config.ts
@@ -3,13 +3,14 @@ import dotenv from "dotenv";
 dotenv.config({ path: ".env" });
 
 export interface Config {
-    port: number;
-    debugLogging: boolean;
-    dbsslconn: boolean;
-    jwtSecret: string;
-    databaseUrl: string;
-    dbEntitiesPath: string[];
-    cronJobExpression: string;
+  port: number;
+  debugLogging: boolean;
+  dbsslconn: boolean;
+  jwtSecret: string;
+  databaseUrl: string;
+  password_hash: string;
+  dbEntitiesPath: string[];
+  cronJobExpression: string;
 }
 
 const isDevMode = process.env.NODE_ENV == "development";
@@ -20,6 +21,7 @@ const config: Config = {
     dbsslconn: !isDevMode,
     jwtSecret: process.env.JWT_SECRET || "your-secret-whatever",
     databaseUrl: process.env.DATABASE_URL || "postgres://user:pass@localhost:5432/apidb",
+    password_hash: process.env.PASSWORD_HASH || 'ultrasecrethash',
     dbEntitiesPath: [
       ... isDevMode ? ["src/entity/**/*.ts"] : ["dist/entity/**/*.js"],
     ],
