16cb6d40e3fa3d33a287d9d8f01a8bdbc40c0654
David Misael Villegas Sandoval <david.misa97@gmail.com>
2021-06-27 06:09:24 -0400

    Updated id variable type and added password to entity and schema

--- src/entity/user.ts
+++ src/entity/user.ts
@@ -1,32 +1,35 @@
 import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
 import { Length, IsEmail } from 'class-validator';
-
 @Entity()
 export class User {
-  @PrimaryGeneratedColumn()
-  id: number;
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
 
-  @Column({ length: 80 })
+  @Column({ length: 80, comment: 'Nombre del usuario' })
   @Length(10, 80)
   name: string;
 
-  @Column({ length: 100 })
+  @Column({ length: 100, comment: 'Correo del usuario' })
   @Length(10, 100)
   @IsEmail()
   email: string;
 
-  @Column({ length: 100 })
+  @Column({
+    length: 100,
+    nullable: false,
+    comment: 'Contraseña del usuario encriptada',
+  })
   @Length(10, 100)
   password: string;
-
 }
 
 export const userSchema = {
-  id: { type: 'number', required: true, example: 1 },
+  id: { type: 'string', required: true },
   name: { type: 'string', required: true, example: 'Javier' },
   email: {
     type: 'string',
     required: true,
     example: 'avileslopez.javier@gmail.com',
   },
+  password: { type: 'string', require: true, example: '********' },
 };
